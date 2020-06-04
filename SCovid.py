d67d8c0fc0987c529c76348b64c9bfd568f7d528
DavidFahrenheit <robeveritt@hotmail.com>
2020-06-04 23:22:17 +0100

    Added comments and changed tanh function

--- SCovid.py
+++ SCovid.py
@@ -9,18 +9,18 @@ from sklearn.linear_model import LinearRegression
 
 scaler = preprocessing.MinMaxScaler()
 
+#Import data files by country
+Bel_dat = pd.read_csv('C:\\Users\\robev\\Documents\\VSCode RME\\Belgium.txt', sep ="\s+")
+Fra_dat = pd.read_csv('C:\\Users\\robev\\Documents\\VSCode RME\\France.txt', sep ="\s+")
+Ger_dat = pd.read_csv('C:\\Users\\robev\\Documents\\VSCode RME\\Germany.txt', sep ="\s+")
+Ita_dat = pd.read_csv('C:\\Users\\robev\\Documents\\VSCode RME\\Italy.txt', sep ="\s+")
+Ned_dat = pd.read_csv('C:\\Users\\robev\\Documents\\VSCode RME\\Netherlands.txt', sep ="\s+")
+Nzl_dat = pd.read_csv('C:\\Users\\robev\\Documents\\VSCode RME\\New Zealand.txt', sep ="\s+")
+Spa_dat = pd.read_csv('C:\\Users\\robev\\Documents\\VSCode RME\\Spain.txt', sep ="\s+")
+Swe_dat = pd.read_csv('C:\\Users\\robev\\Documents\\VSCode RME\\Sweden.txt', sep ="\s+")
+Usa_dat = pd.read_csv('C:\\Users\\robev\\Documents\\VSCode RME\\United States.txt', sep ="\s+")
 
-Bel_dat = pd.read_csv('C:\\Users\\Rob\\Documents\\VSCPython\\Covid19\\Belgium.txt', sep ="\s+")
-Fra_dat = pd.read_csv('C:\\Users\\Rob\\Documents\\VSCPython\\Covid19\\France.txt', sep ="\s+")
-Ger_dat = pd.read_csv('C:\\Users\\Rob\\Documents\\VSCPython\\Covid19\\Germany.txt', sep ="\s+")
-Ita_dat = pd.read_csv('C:\\Users\\Rob\\Documents\\VSCPython\\Covid19\\Italy.txt', sep ="\s+")
-Ned_dat = pd.read_csv('C:\\Users\\Rob\\Documents\\VSCPython\\Covid19\\Netherlands.txt', sep ="\s+")
-Nzl_dat = pd.read_csv('C:\\Users\\Rob\\Documents\\VSCPython\\Covid19\\New Zealand.txt', sep ="\s+")
-Spa_dat = pd.read_csv('C:\\Users\\Rob\\Documents\\VSCPython\\Covid19\\Spain.txt', sep ="\s+")
-Swe_dat = pd.read_csv('C:\\Users\\Rob\\Documents\\VSCPython\\Covid19\\Sweden.txt', sep ="\s+")
-Usa_dat = pd.read_csv('C:\\Users\\Rob\\Documents\\VSCPython\\Covid19\\United States.txt', sep ="\s+")
-
-
+#Define number of rows and cols in each dataframe
 Bel_rows, Bel_cols = Bel_dat.shape
 Fra_rows, Fra_cols = Fra_dat.shape
 Ger_rows, Ger_cols = Ger_dat.shape
@@ -31,6 +31,7 @@ Spa_rows, Spa_cols = Spa_dat.shape
 Swe_rows, Swe_cols = Swe_dat.shape
 Usa_rows, Usa_cols = Usa_dat.shape
 
+#Define start and end rows for each country, for when dataframes are concatenated into one dataframe
 Bel_start = 0
 Bel_end = Bel_rows
 Fra_start = Bel_end
@@ -39,21 +40,29 @@ Ger_start = Fra_end
 Ger_end = Ger_start + Ger_rows
 Ita_start = Ger_end
 Ita_end = Ita_start + Ita_rows
+Ned_start = Ita_end
+Ned_end = Ned_start + Ned_rows
+Nzl_start = Ned_end
+Nzl_end = Nzl_start + Nzl_rows
+Spa_start = Nzl_end
+Spa_end = Spa_start + Spa_rows
+Swe_start = Spa_end
+Swe_end = Swe_start + Swe_rows
+Usa_start = Swe_end 
+Usa_end = Usa_start + Usa_rows
 
-#print("Belgium rows = ", Bel_rows)
-
+#Use pandas concatenate to put all country data into one dataframe
 CV_data = pd.concat((Bel_dat,Fra_dat,Ger_dat,Ita_dat,Ned_dat,Nzl_dat,Spa_dat,Swe_dat,Usa_dat),axis=0)
 
-
-#code below doesn't work 
-#CV_data.drop(columns = 'Date')
-
+#Seperate dataframe into X and Y
 X = CV_data.iloc[:,0:-1]
 Y = CV_data.iloc[:,-1]
 
+#Convert X and Y dataframes into numpy arrays
 Xnp = X.to_numpy()
 Ynp = Y.to_numpy()
 
+#Create more features by multiplying features together - multiply Xnp by each individual Xnp Column (elementwise multiplication)
 X0 = np.multiply(Xnp[0,:],Xnp)
 X1 = np.multiply(Xnp[1,:],Xnp)
 X2 = np.multiply(Xnp[2,:],Xnp)
@@ -61,20 +70,18 @@ X3 = np.multiply(Xnp[3,:],Xnp)
 X4 = np.multiply(Xnp[4,:],Xnp)
 X_mult = np.concatenate((Xnp,X0,X1,X2,X3,X4), axis=1)
 
-#Xnp = np.delete(Xnp1,1,axis =1)
-
+#scale all X_mult columns between 0 and 1
 X_sc = scaler.fit_transform(X_mult)
 
-#print(X_sc)
-
+#Create X^2 and X^3 features
 X2 = scaler.fit_transform(Xnp**2)
 X3 = scaler.fit_transform(Xnp**3)
 
-Xtanh= (np.tanh((X_sc-0.2)/2)+1-(np.tanh((X_sc-0.4)/2)+1))
-X_xxx = np.multiply(X_sc[1,:], Xtanh)
-X_cos = np.cos(X_sc)
-X_sin = np.sin(X_sc)
-XinvLog = scaler.fit_transform(np.log((Xnp+0.1)**(-1)))
+#Create Xtanh features from scaled X values
+# (x*X_sc-y) x compresses shape of function, y offsets shape of function along x-axis
+# +1 offsets shape of funtction along y axis to keep all y values between 0 and 2
+# same term is subtracted with greater offset to create 'hump'
+Xtanh= (np.tanh(3*X_sc-2.5)+1-(np.tanh(3*X_sc-3.5)+1))
 
 X_train = np.concatenate((X_sc,Xtanh), axis =1)
 #X_train = X_sc
@@ -99,33 +106,67 @@ y_pred = linmodel.predict(X_train)
 #np.savetxt('2darray.csv', X_train, delimiter=',', fmt='%d')
 #np.savetxt('thetas.csv', Mtheta, delimiter=',', fmt='%d')
 
-#print(Xnp[:,1].shape)
+#plt.subplot(321)
+#plt.plot(Xnp[Bel_start:Bel_end,0],y_pred[Bel_start:Bel_end], label = "predicted")
+#plt.plot(Xnp[Bel_start:Bel_end,0],Ynp[Bel_start:Bel_end], label = "actual")
+#plt.xlabel('days after 5th case')
+#plt.ylabel('deaths')
+#plt.title('Belgium')
+
+#plt.subplot(322)
+#plt.plot(Xnp[Fra_start:Fra_end,0],y_pred[Fra_start:Fra_end], label = "predicted")
+#plt.plot(Xnp[Fra_start:Fra_end,0],Ynp[Fra_start:Fra_end], label = "actual")
+#plt.xlabel('days after 5th case')
+#plt.ylabel('deaths')
+#plt.title('France')
+
+#plt.subplot(323)
+#plt.plot(Xnp[Ger_start:Ger_end,0],y_pred[Ger_start:Ger_end], label = "predicted")
+#plt.plot(Xnp[Ger_start:Ger_end,0],Ynp[Ger_start:Ger_end], label = "actual")
+#plt.xlabel('days after 5th case')
+#plt.ylabel('deaths')
+#plt.title('Germany')
+
 plt.subplot(321)
-plt.plot(Xnp[Bel_start:Bel_end,0],y_pred[Bel_start:Bel_end], label = "predicted")
-plt.plot(Xnp[Bel_start:Bel_end,0],Ynp[Bel_start:Bel_end], label = "actual")
-plt.xlabel('days after 5th case')
-plt.ylabel('deaths')
-plt.title('Belgium')
-
-plt.subplot(322)
-plt.plot(Xnp[Fra_start:Fra_end,0],y_pred[Fra_start:Fra_end], label = "predicted")
-plt.plot(Xnp[Fra_start:Fra_end,0],Ynp[Fra_start:Fra_end], label = "actual")
-plt.xlabel('days after 5th case')
-plt.ylabel('deaths')
-plt.title('France')
-
-plt.subplot(323)
-plt.plot(Xnp[Ger_start:Ger_end,0],y_pred[Ger_start:Ger_end], label = "predicted")
-plt.plot(Xnp[Ger_start:Ger_end,0],Ynp[Ger_start:Ger_end], label = "actual")
-plt.xlabel('days after 5th case')
-plt.ylabel('deaths')
-plt.title('Germany')
-
-plt.subplot(324)
 plt.plot(Xnp[Ita_start:Ita_end,0],y_pred[Ita_start:Ita_end], label = "predicted")
 plt.plot(Xnp[Ita_start:Ita_end,0],Ynp[Ita_start:Ita_end], label = "actual")
 plt.xlabel('days after 5th case')
 plt.ylabel('deaths')
 plt.title('Italy')
+
+plt.subplot(322)
+plt.plot(Xnp[Ned_start:Ned_end,0],y_pred[Ned_start:Ned_end], label = "predicted")
+plt.plot(Xnp[Ned_start:Ned_end,0],Ynp[Ned_start:Ned_end], label = "actual")
+plt.xlabel('days after 5th case')
+plt.ylabel('deaths')
+plt.title('Netherlands')
+
+plt.subplot(323)
+plt.plot(Xnp[Nzl_start:Nzl_end,0],y_pred[Nzl_start:Nzl_end], label = "predicted")
+plt.plot(Xnp[Nzl_start:Nzl_end,0],Ynp[Nzl_start:Nzl_end], label = "actual")
+plt.xlabel('days after 5th case')
+plt.ylabel('deaths')
+plt.title('New Zealand')
+
+plt.subplot(324)
+plt.plot(Xnp[Spa_start:Spa_end,0],y_pred[Spa_start:Spa_end], label = "predicted")
+plt.plot(Xnp[Spa_start:Spa_end,0],Ynp[Spa_start:Spa_end], label = "actual")
+plt.xlabel('days after 5th case')
+plt.ylabel('deaths')
+plt.title('Spain')
+
+plt.subplot(325)
+plt.plot(Xnp[Swe_start:Swe_end,0],y_pred[Swe_start:Swe_end], label = "predicted")
+plt.plot(Xnp[Swe_start:Swe_end,0],Ynp[Swe_start:Swe_end], label = "actual")
+plt.xlabel('days after 5th case')
+plt.ylabel('deaths')
+plt.title('Sweden')
+
+plt.subplot(326)
+plt.plot(Xnp[Usa_start:Usa_end,0],y_pred[Usa_start:Usa_end], label = "predicted")
+plt.plot(Xnp[Usa_start:Usa_end,0],Ynp[Usa_start:Usa_end], label = "actual")
+plt.xlabel('days after 5th case')
+plt.ylabel('deaths')
+plt.title('Usa')
 
 plt.show()
